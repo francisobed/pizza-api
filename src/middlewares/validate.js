35d6a0a8ac88b24e6152531c984cfdcc6c5d5457
obadiahf <obadiahf@accessbankplc.com>
2021-07-29 12:53:42 +0100

    added the register and get registered user routes

---
+++ src/middlewares/validate.js
@@ -0,0 +1,97 @@
+const Joi = require('joi');
+
+exports.createAccountSchema = (req, res, next) => {
+    // create schema object
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        role: Joi.string().required()
+    });
+
+    // schema options
+    const options = {
+        abortEarly: false, // include all errors
+        allowUnknown: true, // ignore unknown props
+        stripUnknown: true // remove unknown props
+    };
+
+    // validate request body against schema
+    const {
+        error,
+        value
+    } = schema.validate(req.body, options);
+
+    if (error) {
+        // on fail return comma separated errors
+        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+    } else {
+        // on success replace req.body with validated value and trigger next middleware function
+        req.body = value;
+        next();
+    }
+}
+
+
+exports.updateAccountSchema = (req, res, next) => {
+    // define base schema rules
+    const schemaRules = {
+        title: Joi.string().empty(''),
+        firstName: Joi.string().empty(''),
+        lastName: Joi.string().empty(''),
+        email: Joi.string().email().empty(''),
+        password: Joi.string().min(6).empty(''),
+    };
+
+    // conditional schema rule - only admins can update role
+    if (req.user.role === 'Admin') {
+        schemaRules.role = Joi.string().valid('Admin', 'User').empty('');
+    }
+
+    // create schema object with rules
+    const schema = Joi.object(schemaRules)
+        // make confirmPassword required IF password is present
+        .with('password', 'confirmPassword');
+
+    // schema options
+    const options = {
+        abortEarly: false, // include all errors
+        allowUnknown: true, // ignore unknown props
+        stripUnknown: true // remove unknown props
+    };
+
+    // validate request body against schema
+    const {
+        error,
+        value
+    } = schema.validate(req.body, options);
+
+    if (error) {
+        // on fail return comma separated errors
+        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+    } else {
+        // on success replace req.body with validated value and trigger next middleware function
+        req.body = value;
+        next();
+    }
+}
+
+
+
+// exports.requestSample = (req,res,next) => {
+//     const schema = Joi.object({
+//         name :Joi.required(),
+//         email:Joi.string().required(),
+//         username:Joi.string().required(),
+//         password:Joi.string().required()
+//     });
+
+//     const result = schema.validate(req.body,  { allowUnknown: false });
+
+//     if(result.error)
+//         return errorResponse(res, result.error.details[0].message.replace(/['"]/g, ''), 422);
+
+//     next();
+// };
